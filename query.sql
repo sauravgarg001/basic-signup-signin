723647c8b9fa4afcb4cab25c6b4864fbf93cb42b
sauravgarg001 <sauravgarg001@gmail.com>
2020-04-02 12:02:59 +0530

    query modified

--- query.sql
+++ query.sql
@@ -8,8 +8,4 @@ CREATE TABLE `accounts`  (
   `name` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
   `ques` varchar(255) NOT NULL,
-  `img` varchar(255) NOT NULL,
-  `score` int(11) NOT NULL,
-  `easy-score` int(11) NOT NULL,
-  `date` varchar(255) DEFAULT NULL
 ) 
