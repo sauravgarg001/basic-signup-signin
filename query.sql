aa1616cdd3c87c8670a12befeacb0d0f17a4a6d4
sauravgarg001 <sauravgarg001@gmail.com>
2020-04-02 12:05:46 +0530

    modified query

--- query.sql
+++ query.sql
@@ -2,10 +2,10 @@
 -- Table structure for accounts
 -- ----------------------------
 DROP TABLE IF EXISTS `accounts`;
-CREATE TABLE `accounts`  (
-  `email` varchar(255) NOT NULL,
+CREATE TABLE `accounts` (
+  `email` varchar(255) NOT NULL PRIMARY KEY,
   `pno` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
-  `ques` varchar(255) NOT NULL,
-) 
+  `ques` varchar(255) NOT NULL
+)
